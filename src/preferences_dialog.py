04cf62b38bcbba71d4a822817523018d3317146a
Wartybix <34974060+Wartybix@users.noreply.github.com>
2025-11-18 20:03:42 +0000

    Remove popovers in preferences dialog

--- src/preferences_dialog.py
+++ src/preferences_dialog.py
@@ -28,26 +28,17 @@ class PreferencesDialog(Adw.PreferencesDialog):
     """ The application's preferences dialog """
     __gtype_name__ = "PreferencesDialog"
 
-    hw_accel_info_popover = Gtk.Template.Child()
-    hw_accel_info_label = Gtk.Template.Child()
-    suffix_info_popover = Gtk.Template.Child()
-    suffix_info_label = Gtk.Template.Child()
     suffix_entry_row = Gtk.Template.Child()
     gpu_encoding_row = Gtk.Template.Child()
+    hw_accel_group = Gtk.Template.Child()
+    suffix_group = Gtk.Template.Child()
 
     # TODO: Maybe do add a megabyte/mebibyte preference.
 
     def __init__(self, application: Adw.Application, **kwargs: Any) -> None:
         super().__init__(**kwargs)
 
-        self.hw_accel_info_label.set_label(
-            # Please use “” instead of "", if applicable to your language.
-            _("Hardware acceleration is only used if your GPU supports encoding with the selected video codec, and “Extra Quality” is disabled.")
-        )
-
-        self.suffix_info_label.set_label(
-            # TRANSLATORS: {} represents the value of the default suffix.
-            # Please use “” instead of "", if applicable to your language.
+        self.suffix_group.set_description(
             _('Used in file names for exported videos, between the base name and extension. If the custom suffix is left empty, the default suffix of “{}” will be used.')
                 .format(application.default_suffix)
         )
@@ -61,32 +52,10 @@ class PreferencesDialog(Adw.PreferencesDialog):
             Gio.SettingsBindFlags.DEFAULT
         )
 
-        self.hw_accel_info_popover.connect(
-            'show',
-            self.read_hw_accel_info_popover
-        )
-        self.suffix_info_popover.connect('show', self.read_suffix_info_popover)
-
         export_suffix_value = self.settings.get_string('custom-export-suffix')
         self.suffix_entry_row.set_text(export_suffix_value)
 
         self.suffix_entry_row.connect('apply', self.update_custom_suffix)
-
-    def read_hw_accel_info_popover(self, widget: Gtk.Widget, *args: Any):
-        """ Use the screen reader to announce the contents of the
-        'hardware acceleration' info popover.
-        """
-        message = self.hw_accel_info_label.get_text()
-
-        self.announce(message, Gtk.AccessibleAnnouncementPriority.MEDIUM)
-
-    def read_suffix_info_popover(self, widget: Gtk.Widget, *args: Any):
-        """ Use the screen reader to announce the contents of the
-        'exported video suffix' info popover.
-        """
-        message = self.suffix_info_label.get_text()
-
-        self.announce(message, Gtk.AccessibleAnnouncementPriority.MEDIUM)
 
     def update_custom_suffix(self, widget: Gtk.Widget) -> None:
         """ Set a new exported file suffix to the application's settings """
